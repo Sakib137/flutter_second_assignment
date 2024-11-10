932b1ddf7c349ddea1d8418d6aeda378321c088f
Sakib137 <mdsakib124202@gmail.com>
2024-11-11 00:13:56 +0600

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyWidget(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("hello"),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
