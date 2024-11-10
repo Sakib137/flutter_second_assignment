b0a989743ac02f5638670df59a7156fb39d838dd
Sakib137 <mdsakib124202@gmail.com>
2024-11-11 01:25:52 +0600

    appbar completed

--- lib/main.dart
+++ lib/main.dart
@@ -22,10 +22,26 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("hello"),
-        backgroundColor: Colors.green,
+        appBar: AppBar(
+      title: const Row(
+        children: [
+          Flexible(
+              child: Align(
+            alignment: Alignment.bottomLeft,
+            child: Text("My App"),
+          ))
+        ],
       ),
-    );
+      actions: [
+        IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.call)),
+      ],
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(16), topRight: Radius.circular(16))),
+      backgroundColor: const Color.fromARGB(255, 228, 188, 8),
+      toolbarHeight: 70,
+    ));
   }
 }
