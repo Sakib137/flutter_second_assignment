1920efcbd129d56192bd32bdcedcc87cbaa8be68
Sakib137 <mdsakib124202@gmail.com>
2024-11-12 21:21:08 +0600

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -43,83 +43,101 @@ class MyWidget extends StatelessWidget {
           backgroundColor: const Color.fromARGB(255, 228, 188, 8),
           toolbarHeight: 70,
         ),
-        body: const Center(
-          child: Column(
-            children: [
-              Align(
-                alignment: Alignment.topCenter,
-                child: Column(
-                  children: [
-                    // For Icecream icon
+        body: ListView(
+          children: const [
+            Center(
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Column(
+                      children: [
+                        // For Icecream icon
 
-                    SizedBox(
-                      height: 16,
-                    ),
-                    CircleAvatar(
-                      backgroundColor: Color.fromARGB(255, 232, 221, 221),
-                      radius: 64,
-                      child: Icon(
-                        Icons.icecream_outlined,
-                        size: 60.0,
-                      ),
-                    ),
-                    Text(
-                      "Ice cream is a very delicious right?",
-                      style: TextStyle(fontWeight: FontWeight.w900),
-                    ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        CircleAvatar(
+                          backgroundColor: Color.fromARGB(255, 232, 221, 221),
+                          radius: 80,
+                          child: Icon(
+                            Icons.icecream_outlined,
+                            size: 80.0,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(24, 8, 24, 8),
+                          child: Text(
+                            "Ice cream is a very delicious right?",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w900, fontSize: 16),
+                          ),
+                        ),
 
-                    // For navigate icon
+                        // For navigate icon
 
-                    SizedBox(
-                      height: 32,
-                    ),
-                    CircleAvatar(
-                      radius: 64,
-                      backgroundColor: Color.fromARGB(255, 232, 221, 221),
-                      child: Stack(
-                        alignment: Alignment.topCenter,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        SizedBox(
+                          height: 32,
+                        ),
+                        CircleAvatar(
+                          radius: 80,
+                          backgroundColor: Color.fromARGB(255, 232, 221, 221),
+                          child: Stack(
+                            alignment: Alignment.topCenter,
                             children: [
-                              Icon(
-                                Icons.navigate_before,
-                                size: 60.0,
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Icon(
+                                    Icons.navigate_before,
+                                    size: 80.0,
+                                  ),
+                                  Icon(
+                                    Icons.navigate_next,
+                                    size: 80.0,
+                                  )
+                                ],
                               ),
-                              Icon(
-                                Icons.navigate_next,
-                                size: 60.0,
-                              )
                             ],
                           ),
-                        ],
-                      ),
-                    ),
-                    Text(
-                      "Programing is not boring if you love it",
-                      style: TextStyle(fontWeight: FontWeight.w900),
-                    ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(24, 8, 24, 8),
+                          child: Text(
+                            "Programing is not boring if you love it",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w900, fontSize: 16),
+                          ),
+                        ),
 
-                    SizedBox(
-                      height: 32,
+                        // For water drop icon
+                        SizedBox(
+                          height: 32,
+                        ),
+                        CircleAvatar(
+                          backgroundColor: Color.fromARGB(255, 232, 221, 221),
+                          radius: 80,
+                          child: Icon(
+                            Icons.water_drop_outlined,
+                            size: 80.0,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(24, 8, 24, 8),
+                          child: Text(
+                            "If you submit code directly copy from chatgpt then mark will 0",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w900, fontSize: 16),
+                          ),
+                        ),
+                      ],
                     ),
-                    CircleAvatar(
-                      backgroundColor: Color.fromARGB(255, 232, 221, 221),
-                      radius: 64,
-                      child: Icon(
-                        Icons.water_drop_outlined,
-                        size: 60.0,
-                      ),
-                    ),
-                    Text(
-                      "If you submit code directly copy from chatgpt then mark will 0",
-                      style: TextStyle(fontWeight: FontWeight.w900),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ));
   }
 }
